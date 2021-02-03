180ef994de54f1d84c88bc9042d667760f91e3c1
roodrigo <roodrigoroot@gmail.com>
2021-02-03 10:59:35 -0600

    Login, Logout added

---
+++ accounts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Company(models.Model):
+
+    name = models.CharField(verbose_name="Compañia", max_length=150)
+    description = models.TextField(verbose_name="Descripción")
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Company"
+        verbose_name_plural = "Companies"
